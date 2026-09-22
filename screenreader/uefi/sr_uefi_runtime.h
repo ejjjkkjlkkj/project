ef01d3de9bd16bc2de83a79c32c5114dad39e284
ejjjkkjlkkj <ism3pro@outlook.com>
2026-09-22 11:31:01 +0200

    Allow realtime runtime to bind standard HII config backend

--- screenreader/uefi/sr_uefi_runtime.h
+++ screenreader/uefi/sr_uefi_runtime.h
@@ -6,6 +6,7 @@
 #include "sr_voice_stream.h"
 #include "sr_uefi_input.h"
 #include "sr_chooser.h"
+#include "sr_uefi_config.h"
 
 typedef int (*sr_activate_fn)(void *ctx, const sr_node *node);
 
@@ -14,6 +15,7 @@ typedef struct {
     sr_voice_stream voice;
     sr_uefi_keyboard keyboard;
     sr_chooser chooser;
+    sr_uefi_config *config;
     sr_activate_fn activate;
     void *activate_ctx;
     sr_u32 audio_frame_budget;
@@ -30,6 +32,9 @@ int sr_uefi_runtime_init(sr_uefi_runtime *runtime,
                          sr_audio_sink audio,
                          sr_activate_fn activate,
                          void *activate_ctx);
+
+void sr_uefi_runtime_bind_config(sr_uefi_runtime *runtime,
+                                 sr_uefi_config *config);
 
 int sr_uefi_runtime_tick(sr_uefi_runtime *runtime);
 
