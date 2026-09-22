2db4334e16b00b87cacde15a4d2ff3dd57ccb0bc
ejjjkkjlkkj <ism3pro@outlook.com>
2026-09-22 11:18:45 +0200

    Model HII question bindings and selectable options

--- screenreader/uefi/sr_hii_ifr.h
+++ screenreader/uefi/sr_hii_ifr.h
@@ -9,6 +9,24 @@ typedef int (*sr_hii_string_resolver)(void *ctx,
                                       sr_u32 out_cap);
 
 typedef struct {
+    sr_u64 value;
+    const char *label;
+    sr_u8 width;
+    sr_u8 flags;
+} sr_hii_option;
+
+typedef struct {
+    sr_u16 question_id;
+    sr_u16 varstore_id;
+    sr_u16 varstore_info;
+    sr_u16 option_start;
+    sr_u8 option_count;
+    sr_u8 opcode;
+    sr_u8 question_flags;
+    sr_u8 oneof_flags;
+} sr_hii_binding;
+
+typedef struct {
     sr_node *nodes;
     sr_u32 node_capacity;
     sr_u32 node_count;
@@ -17,6 +35,11 @@ typedef struct {
     sr_u32 string_used;
     sr_hii_string_resolver resolve_string;
     void *resolve_ctx;
+    sr_hii_binding *bindings;
+    sr_u32 binding_capacity;
+    sr_hii_option *options;
+    sr_u32 option_capacity;
+    sr_u32 option_count;
     sr_u32 malformed_opcodes;
     sr_u32 dropped_nodes;
 } sr_hii_model;
@@ -33,8 +56,18 @@ void sr_hii_model_init(sr_hii_model *model,
  * Parse one EFI_HII_PACKAGE_FORMS package including its four-byte package
  * header. Appends nodes to model and never allocates.
  */
+void sr_hii_model_attach_metadata(sr_hii_model *model,
+                                  sr_hii_binding *bindings,
+                                  sr_u32 binding_capacity,
+                                  sr_hii_option *options,
+                                  sr_u32 option_capacity);
+
 int sr_hii_parse_forms_package(sr_hii_model *model,
                                const sr_u8 *package,
                                sr_u32 package_bytes);
+
+int sr_hii_apply_raw_value(sr_hii_model *model,
+                           sr_u32 node_index,
+                           sr_u64 raw_value);
 
 #endif
